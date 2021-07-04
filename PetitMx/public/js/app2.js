8c6a96b4817cd734e55978ffc1fac69fc37cd519
chentitorupa <ruizvicente020@gmail.com>
2021-07-04 02:11:16 -0500

    Se realizó un preload para el sitio

--- PetitMx/public/js/app2.js
+++ PetitMx/public/js/app2.js
@@ -1,3 +1,8 @@
+window.addEventListener('load', () =>{
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-finish');
+});
+
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.dropdown-trigger').dropdown();
