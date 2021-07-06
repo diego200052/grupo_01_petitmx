18884e3ee6dd3e6195e3ae627e598cdeb2bbd0ad
chentitorupa <ruizvicente020@gmail.com>
2021-07-05 20:39:05 -0500

    Se agregó página de error 404

--- PetitMx/app.js
+++ PetitMx/app.js
@@ -21,3 +21,7 @@ app.listen(3030,() => console.log('Server running in 3030 port'));
 app.use('/', mainRoutes);
 app.use('/users', usersRoutes);
 app.use('/products', productsRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render("not-found");
+})
