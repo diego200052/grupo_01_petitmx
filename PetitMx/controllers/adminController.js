294167a01f7a897ad1330d42011d889e575daaf0
Diego Medina <com1pac56@hotmail.com>
2021-07-27 20:47:56 -0500

    Formulario funcional para la creación y edición de productos

--- PetitMx/controllers/adminController.js
+++ PetitMx/controllers/adminController.js
@@ -7,11 +7,78 @@ let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const controlador = {
     editProduct: (req, res) => {
-        res.render('admin/editProduct');
+		const producto = products.find( (producto) => {
+			return producto.id == req.params.id;
+		});
+        res.render('admin/editProduct', { producto:producto });
     },
+
+	// Update - Method to update
+	updateProduct: (req, res) => {
+		const file = req.file;
+		if(!file) {
+			products.forEach( (producto) => {
+				if(producto.id == req.params.id) {
+					
+					producto.nombre = req.body.productName;
+					producto.marca = req.body.brand;
+					producto.precio = req.body.price;
+					producto.descripcion = req.body.description;
+					producto.caracteristicas = req.body.features;
+					producto.instrucciones = req.body.instructions;
+					producto.categoria = req.body.category;
+					
+				}
+			});
+		}
+		else {
+			products.forEach( (producto) => {
+				if(producto.id == req.params.id) {
+					
+					producto.nombre = req.body.productName;
+					producto.marca = req.body.brand;
+					producto.precio = req.body.price;
+					producto.descripcion = req.body.description;
+					producto.caracteristicas = req.body.features;
+					producto.instrucciones = req.body.instructions;
+					producto.imagen = file.filename;
+					producto.categoria = req.body.category;
+					
+				}
+			});
+		}
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+		res.redirect('/products');
+	},
+
     addProduct: (req, res) => {
         res.render('admin/addProduct');
     },
+
+	// Create -  Method to store
+	storeProduct: (req, res) => {
+		const file = req.file;
+		if(!file) {
+		  const error = new Error('Por favor, seleccione un archivo');
+		  error.httpStatusCode = 404;
+		  return next(error);
+		}
+
+		let producto = {
+			id: products[products.length-1].id+1,
+			nombre: req.body.productName,
+			marca: req.body.brand,
+			precio: req.body.price,
+			descripcion: req.body.description,
+			caracteristicas: req.body.features,
+			instrucciones: req.body.instructions,
+			imagen: file.filename,
+			categoria: req.body.category
+		};
+		products.push(producto);
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+		res.redirect('/products');
+	},
 
     // Delete - Delete one product from DB
 	destroyProduct : (req, res) => {
--- PetitMx/routes/admin.js
+++ PetitMx/routes/admin.js
@@ -3,16 +3,31 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, path.join(__dirname, '../public/img/productos'))
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+	}
+});
+
+var upload = multer({ storage: storage });
+
 // ************ Controller Require ************
 const adminController = require('../controllers/adminController');
 
 /*** GET ALL PRODUCTS ***/
 
 /*** CREATE ONE PRODUCT ***/
-router.get('/addProduct/:id', adminController.addProduct);
+router.get('/addProduct', adminController.addProduct);
+
+router.post('/addProduct', upload.single('image'), adminController.storeProduct);
 
 /*** EDIT ONE PRODUCT ***/
 router.get('/editProduct/:id', adminController.editProduct);
+
+router.post('/editProduct/:id', upload.single('image'), adminController.updateProduct);
 
 /*** DELETE ONE PRODUCT***/
 router.delete('/deleteProduct/:id', adminController.destroyProduct);
