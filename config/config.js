f7b1551cda38c7ad91b2724235a86279f844388b
Diego Medina <com1pac56@hotmail.com>
2021-10-17 20:05:18 -0500

    Password update

--- config/config.js
+++ config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DBUSERNAME || "digitalhouse",
+    "password": process.env.DBPASSWORD ||"digitalhouse",
+    "database": process.env.DBNAME || "petit_db",
+    "host": process.env.DBHOST || "127.0.0.1",
     "dialect": "mysql"
   }
 }
