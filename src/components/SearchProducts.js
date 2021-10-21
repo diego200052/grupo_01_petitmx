871ddb0ae033e60d111afd1eb8bb42f7a0a17a19
Diego Medina <com1pac56@hotmail.com>
2021-10-21 13:18:49 -0500

    corrección num search dashboard

--- src/components/SearchProducts.js
+++ src/components/SearchProducts.js
@@ -74,7 +74,7 @@ function SearchProducts(){
 				</div>
 			</div>
             {
-				keyword <= (productCount+5)*10 && keyword > 0 ?
+				keyword <= ((productCount*10)-5) && keyword > 0 ?
 				<>
                     <div className="row">
                         <div className="col-12">
